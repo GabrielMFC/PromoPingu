1401a02999e7d5cb3a3b4db8ea7824afb5edded3
Gabriel Matias <ggezcom991gh@gmail.com>
2025-04-16 16:30:48 -0300

    Otimizando o código do bot.

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits, Options } = require('discord.js')
 require("dotenv").config()
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ],
+
+    makeCache: Options.cacheWithLimits({
+        MessageManager: 0,
+        ThreadManager: 0,
+        GuildEmojiManager: 0,
+        ReactionManager: 0,
+        PresenceManager: 0,
+        GuildBanManager: 0,
+        VoiceStateManager: 0,
+        StageInstanceManager: 0,
+        GuildInviteManager: 0,
+        ApplicationCommandManager: 0,
+        BaseGuildEmojiManager: 0,
+        GuildMemberManager: 10
+    })
+
 })
 
-client.once("ready", () => {
-    console.log(`✅ Bot online como ${client.user.tag}`)
+client.on("guildMemberAdd", async member => {
+    const channel = member.guild.channels.cache.get("1348770833388015629")
+    const roleId = "1361849497889669220"
+    
+    await member.roles.add(roleId)
+
+    if (channel && channel.permissionsFor(member.guild.members.me).has("SendMessages")) {
+        channel.send(`Bem vindo ao server, ${member.user}! \n ${member.user} agora é um dos nossos colaboradores!`)
+    }
 })
 
 client.login(process.env.TOKEN)
